6ff8845e494800559af6f64dd03055ac4366391a
Christian Loza <Christianlzap@gmail.com>
2021-09-12 15:25:37 -0500

    Sidebar correction colors

--- front-end/tutorial-canciones/src/app/ui/sidebar/sidebar.component.ts
+++ front-end/tutorial-canciones/src/app/ui/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-sidebar',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private routerPath: Router,
+    private router: ActivatedRoute) {
+  }
 
   ngOnInit(): void {
+  }
+
+  goTo(menu: string){
+    if(menu === "logIn"){
+      this.routerPath.navigate([`/signin`])
+    }
+    else if(menu === "album"){
+      this.routerPath.navigate([`/albumes`])
+    }
+    else if(menu === "cancion"){
+      this.routerPath.navigate([`/canciones`])
+    }
+    else{
+      this.routerPath.navigate([`/about`])
+    }
   }
 
 }
