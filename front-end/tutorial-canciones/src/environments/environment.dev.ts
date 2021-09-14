f6312317a41d8040262d101b38d4e2ee1e38ccca
José López <jl.lopez77@uniandes.edu.co>
2021-09-14 10:00:18 -0500

    Update environment.dev.ts

--- front-end/tutorial-canciones/src/environments/environment.dev.ts
+++ front-end/tutorial-canciones/src/environments/environment.dev.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   title: 'Dev Enviroment Heading',
-  baseUrl:"https://test-back-lonic-27.herokuapp.com"
+  baseUrl:"https://back-dev-lonic-27.herokuapp.com"
 };
